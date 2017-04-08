6b9c537317fdf948bddfd7393807ed468e042ab7
radioflygit <radiofly19841018@gmail.com>
2017-04-08 17:33:45 +0800

    添加地图,替换引用组件方式

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
+// import Hello from '@/components/Hello'
 Vue.use(Router)
 
 export default new Router({
@@ -9,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: require('../components/Hello')
     }
   ]
 })
